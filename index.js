6b2839690409b8d2b42c02e6987ca2b986f4d321
Krzysztof Czopp <chris@glue.codes>
2021-01-20 17:50:17 +0000

    create prerenderer

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 export { default as createHostComponent } from './src/createHostComponent'
 export { default as createPageInitializer } from './src/createPageInitializer'
 export { default as createPrerenderComponent } from './src/createPrerenderComponent'
+export { default as createPrerenderer } from './src/createPrerenderer'
 export { default as createRenderer } from './src/createRenderer'
---
+++ src/createPrerenderer.js
@@ -0,0 +1,53 @@
+const selfClosingTags = ['br', 'img', 'hr', 'input', 'source']
+
+function flattenChildren (htmlChunks) {
+  return htmlChunks.map((child) => {
+    let transformedChild = child
+
+    if (Array.isArray(child)) {
+      transformedChild = flattenChildren(child).join('')
+    }
+
+    return transformedChild
+  })
+}
+
+const camelcaseToDashed = string => string.replace(/[A-Z]/g, c => `-${c.toLowerCase()}`)
+
+const styleObjectToString = (styleObject) => {
+  if (typeof styleObject === 'string') {
+    return styleObject
+  }
+
+  return Object.keys(styleObject)
+    .map(propName => `${camelcaseToDashed(propName)}:${styleObject[propName]}`)
+    .join(';')
+}
+
+const stringifyNode = (name, props, children) => {
+  const attrs = Object.keys(props)
+    .reduce((acc, propName) => {
+      if (propName === 'className') {
+        acc.push(`class="${props[propName]}"`)
+      } else if (propName === 'attributes') {
+        Object.keys(props[propName]).forEach((attrName) => {
+          acc.push(`${attrName}="${props[propName][attrName]}"`)
+        })
+      } else if (propName === 'style') {
+        acc.push(`${propName}="${styleObjectToString(props[propName])}"`)
+      } else if (!/^on[a-z]+$/.test(propName) && !['gc-onceDomNodeVisited', 'gc-onDomNodeConnected'].includes(propName)) {
+        acc.push(`${propName}="${props[propName]}"`)
+      }
+
+      return acc
+    }, [])
+    .join(' ')
+
+  if (selfClosingTags.includes(name)) {
+    return `<${name}${attrs ? ` ${attrs}` : ''}/>`
+  }
+
+  return `<${name}${attrs ? ` ${attrs}` : ''}>${children.join('')}</${name}>`
+}
+
+export default () => (name, props, ...children) => stringifyNode(name, props || {}, flattenChildren(children))
