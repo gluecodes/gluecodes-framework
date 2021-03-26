d70f24ce9ee34bc9bb2679cf9822f12d668eaa41
Krzysztof Czopp <chris@glue.codes>
2021-03-26 14:13:15 +0000

    fix hyperscript bug

--- src/createRenderer.js
+++ src/createRenderer.js
@@ -1,6 +1,14 @@
+const filterAttrs = attrs => Object.keys(attrs).reduce((acc, attrName) => {
+  if (!/^gc-/.test(attrName)) {
+    acc[attrName] = attrs[attrName]
+  }
+
+  return acc
+}, {})
+
 const rewriteProps = props => Object.keys(props || {}).reduce((acc, propName) => ({
   ...acc,
-  ...(propName === 'attrs' ? { attrs: undefined, ...props[propName] } : {})
+  ...(propName === 'attrs' ? { attrs: undefined, ...filterAttrs(props[propName]) } : {})
 }), props)
 
 export default hyperScript => (tagName, props, ...children) => {
@@ -8,5 +16,11 @@ export default hyperScript => (tagName, props, ...children) => {
     return children
   }
 
-  return hyperScript(tagName, rewriteProps(props), children)
+  const anyReactiveChildren = children.some(node => typeof node === 'function')
+
+  if (anyReactiveChildren) {
+    return hyperScript(tagName, rewriteProps(props), () => children)
+  }
+
+  return hyperScript(tagName, rewriteProps(props), ...children)
 }
