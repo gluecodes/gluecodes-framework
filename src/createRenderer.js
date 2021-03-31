18c499105140c43c72d96975dc778273f36894e8
Krzysztof Czopp <chris@glue.codes>
2021-03-31 11:01:50 +0100

    fragment in renderer

--- src/createRenderer.js
+++ src/createRenderer.js
@@ -15,7 +15,7 @@ const rewriteProps = props => Object.keys(props || {}).reduce((acc, propName) =>
 }), props)
 
 export default hyperScript => (tagName, props, ...children) => {
-  if (props && props.attributes && props.attributes['gc-as'] === 'widget') {
+  if (tagName === 'fragment' || (props && props.attrs && props.attrs['gc-as'] === 'widget')) {
     return children
   }
 
